5e6b07889c093f9a5613ff96dcc473b8519237d5
reybans <2225189@slu.edu.ph>
2023-11-27 15:17:00 +0800

    login updates, session handling stuff, kain muna

--- admin/scripts/login.js
+++ admin/scripts/login.js
@@ -1,5 +1,32 @@
+var usernameInput = document.getElementById("usernameInput");
+var passwordInput = document.getElementById("passwordInput");
+
+function verifyLogin() {
+    var usernameValue = usernameInput.value;
+    var passwordValue = passwordInput.value;
+
+    fetch(`/verify?username=${usernameValue}&password=${passwordValue}`)
+    .then(response => {
+        if (response.ok) {
+            window.location.href = '/dashboard';
+        } else {
+            return response.json().then(data => {
+                throw new Error(data.message); 
+            });
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error.message);
+        alert(error.message);
+    });
+}
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    verifyLogin();
+});
+
 document.addEventListener("DOMContentLoaded", function() {
-    var passwordInput = document.getElementById("passwordInput");
     var eyeBtn = document.getElementById("showPass");
     var btnCheck = false;
 
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express')
+const session = require('express-session')
 const { request } = require('http')
 const path = require('path')
-const app = express()
 const mysql = require('mysql')
 const port = 3000
+
+const app = express()
 
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'wibtik'
+  database: 'webteknameronadmin'
 });
 
 connection.connect((err) => {
@@ -20,44 +22,69 @@ connection.connect((err) => {
   console.log('Connected to MySQL database successfully!')
 })
 
-app.set('view engine', 'ejs')
-
-app.use('/scripts', express.static(path.join(__dirname, 'scripts')))
-
-app.get('/', (req, res) => {
-  res.render("../admin/login.ejs")
-})
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
   console.log(`http://localhost:${port}/`)
 })  
 
+app.set('view engine', 'ejs')
+
+app.use('/admin/scripts', express.static(path.join(__dirname, 'admin/scripts')))
+
+app.use(session({
+  secret: 'idk',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.get('/', (req, res) => {
+  res.render("../admin/login.ejs");
+})
+
 app.get('/login', (req, res) => {
-    res.render('../admin/login.ejs')
-    res.send('naklik jay login')
-  })
+    res.render('../admin/login.ejs');
+})
 
-  app.get('/verify', (req, res) => {
-    let username = req.query.username;
-    let password = req.query.password;
+app.get('/verify', (req, res) => {
+  let username = req.query.username;
+  let password = req.query.password;
 
-    connection.query(
-      'SELECT * FROM admin WHERE username = ? AND password = ?',
-      [username, password],
-      (error, results, fields) => {
-        if(error) {
-          console.error('Error querying database:', error);
-          res.status(500).send('Error verifying credentials!');
-        }
-
-        if (results.length > 0) {
-          // yes user
-          res.status(200).send('<h1>Hello, ${username}! Welcome!</h1>');
-        } else {
-          // non user
-          res.status(401).send('<h1>Invalid username or password</h1>');
-        }
+  connection.query(
+    'SELECT * FROM admin WHERE username = ? AND password = ?',
+    [username, password],
+    (error, results, fields) => {
+      if(error) {
+        console.error('Error querying database:', error);
+        res.status(500).send('Error verifying credentials!');
       }
-    )
-  });
+
+      if (results.length > 0) {
+        // yes user
+        req.session.username = username;
+        res.redirect('/dashboard');
+      } else {
+        // non user
+        res.status(401).json( {message: 'Invalid username or password! Try again. tite' });
+      }
+    }
+  )
+});
+
+app.get('/dashboard', (req, res) => {
+  if (req.session.username) {
+    res.render('../admin/dashboard.ejs');
+  } else {
+    res.redirect('/login');
+  }
+});
+
+app.post('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.error("Error destroying session road:", err);
+      res.status(500).send('Error logging out')
+    } else {
+      res.redirect('/login');
+    }
+  })
+})

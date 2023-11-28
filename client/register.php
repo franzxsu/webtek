e2fbc5594c0348269ae2890c4b2e6dbaf6ba0069
2205466 <2205466@slu.edu.ph>
2023-11-28 22:02:36 +0800

    Error handling

--- client/register.php
+++ client/register.php
@@ -8,7 +8,6 @@ include("database_handler.php");
     // INSERT INTO `users`(email, password) VALUES ("asd", "asd");
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
         //check kung nalagyan ng shit sa lahat ng fields, todo add more validators
         if(!empty($_POST['first_name']) && 
             !empty($_POST['last_name']) && 
@@ -16,28 +15,30 @@ include("database_handler.php");
             !empty($_POST['password']) && 
             !empty($_POST['password_repeat'])){
 
+            if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                header("Location: register.php?signup=invalidemail");
+            } else {
                 $email = $_POST['email'];
                 $firstName = $_POST['first_name'];
                 $lastName = $_POST['last_name'];
                 $password = $_POST['password'];
-
                 $result = add_user($email, $firstName, $lastName, $password);
-
                 if($result !== true){
                     echo "error: " . $result;  
-                  } else {
+                } else {
                     //REGISTRATION SUCCESSFUL, GO TO LOGIN
                     header("Location: login.php");
-                    exit;
-                  }
-
-                
-            } else {
-                echo "INVALID";
-            }
-            
-
-    }
+                    exit();
+                }
+            }     
+        } else (empty($_POST['first_name']) && 
+                empty($_POST['last_name']) && 
+                empty($_POST['email']) && 
+                empty($_POST['password']) && 
+                empty($_POST['password_repeat'])) {
+            header('Location: register.php?signup=empty')
+        }
+    
 ?>
 
 <!DOCTYPE html>
@@ -63,7 +64,7 @@ include("database_handler.php");
                             <div class="text-center">
                                 <h4 class="text-dark mb-4">Sign In</h4>
                             </div>
-                            <form class="user" method="post">
+                            <form class="user" action="reg_error_hand.php" method="post">
                                 <div class="row mb-3">
                                     <div class="col-sm-6 mb-3 mb-sm-0"><input class="form-control form-control-user" type="text" id="exampleFirstName" placeholder="First Name" name="first_name"></div>
                                     <div class="col-sm-6"><input class="form-control form-control-user" type="text" id="exampleLastName" placeholder="Last Name" name="last_name"></div>
@@ -75,6 +76,15 @@ include("database_handler.php");
                                 </div><button class="btn btn-primary d-block btn-user w-100" type="submit">Register Account</button>
                                 <hr>
                             </form>
+
+                            <?php
+                                $fullUrl = "http://$_SERVER[HTTP_POST]$_SERVER[REQUEST_URI]";
+
+                                if (strpos($fullUrl, "signup=empty") == true) {
+                                    echo "Please fill in all fields.";
+                                }
+                            ?>
+
                             <div class="text-center"></div>
                             <div class="text-center"><a class="small" href="login.php">Already have an account? Login!</a></div>
                         </div>

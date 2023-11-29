c95e1b36451ef8f8092722823ff392996623e559
derekisabelo <derekisabelo07@gmail.com>
2023-11-29 21:37:03 +0800

    Separated php code to a different file in index.php

--- client/index.php
+++ client/index.php
@@ -152,38 +152,7 @@ if (isset($_SESSION['user_id'])) {
                         <h3 class="text-dark mb-0">Upcoming Events</h3><a class="btn btn-primary btn-sm d-none d-sm-inline-block" role="button" href="#"><i class="fas fa-download fa-sm text-white-50"></i>&nbsp;Generate Report</a>
                     </div>
                     <div class="row">
-                    <?php
-                        $sql = "SELECT * FROM events";
-                        $result = $conn->query($sql);
-                        
-                        if ($result->num_rows > 0) {
-                            while ($row = $result->fetch_assoc()) {
-                                $eventName = $row["EventName"];
-                                $eventDateStart = $row["EventDateStart"];
-                                $eventDateEnd = $row["EventDateEnd"];
-                                $eventLocation = $row["EventLocation"];
-                                $eventInfo = $row["EventInfo"];
-                        
-                                echo '<div class="col-md-6 col-xl-3 mb-4">
-                                        <div class="card" style="width: 18rem; height: 35rem;">
-                                            <h5 class="card-title d-flex justify-content-center" style="padding: 15px; padding-bottom: 5px; text-align: center;">
-                                                ' . $eventName . '
-                                            </h5>
-                                            <img class="card-img-top img-fluid" src="../client/assets/img/sample_pubmat.jpg" alt="Card image cap" style="border-radius: 0px; max-width: 100%; height: 14rem; object-fit: cover;">
-                                            <div class="card-body" style="padding: 0px;">
-                                                <p class="card-text" style="margin: 0px; text-align: center;">DATE: ' . $eventDateStart . ' - ' . $eventDateEnd . '</p>
-                                                <p class="card-text" style="margin: 0px; text-align: center;">VENUE: ' . $eventLocation . '</p>
-                                            </div>
-                                            <div class="card-body" style="max-height: 500px; overflow-y: auto;">
-                                                <p class="card-text" style="max-width: 100%; padding: 0px;">' . $eventInfo . '</p>
-                                            </div>
-                                            <a href="#" class="btn btn-primary" style="border-radius: 0px;">Register</a>
-                                        </div>
-                                    </div>';
-                            }
-                        }
-                        ?>
-
+                        <?php include 'event_container_gen.php' ?>
                     </div>
                 </div>
             </div>

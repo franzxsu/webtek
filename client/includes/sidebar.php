071bcaef56757e77af5da8018e5653f48384139e
franzxsu <2225292@slu.edu.ph>
2023-12-02 15:56:15 +0800

    minor ui cleanup - sidebar, guest side.

--- client/includes/sidebar.php
+++ client/includes/sidebar.php
@@ -7,13 +7,13 @@
                 </a>
                 <hr class="sidebar-divider my-0">
                 <ul class="navbar-nav text-light" id="accordionSidebar">
-                    <li class="nav-item"><a class="nav-link active" href="index.php"><i class="fas fa-tachometer-alt"></i><span>Dashboard</span></a></li>
+                    <li class="nav-item"><a class="nav-link active" href="index.php"><i class="fa fa-calendar"></i><span>Upcoming events</span></a></li>
 
                     <?php
 
                     if(isset($_SESSION['user_id'])){
-                        echo ' <li class="nav-item"><a class="nav-link" href="my_events.php"><i class="fas fa-user"></i><span>My Events</span></a></li> ';
-                        echo ' <li class="nav-item"><a class="nav-link" href="history_events.php"><i class="fas fa-user"></i><span>Past Events</span></a></li>';
+                        echo ' <li class="nav-item"><a class="nav-link" href="my_events.php"><i class="fas fa-calendar-alt"></i><span>My Events</span></a></li> ';
+                        echo ' <li class="nav-item"><a class="nav-link" href="history_events.php"><i class="far fa-calendar-check"></i><span>Past Events</span></a></li>';
                     }
 
                     ?>

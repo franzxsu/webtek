6ffee1c6eca2748631138d8d14cee3d5022144f7
reybans <2225189@slu.edu.ph>
2023-12-07 22:51:03 +0800

    returned jquery

--- .backupfiles/scripts/drag_and_drop.js
+++ .backupfiles/scripts/drag_and_drop.js
@@ -1,30 +1,30 @@
 // jQuery
-// $(document).on('change', '.file-input', function() {
-//     var filesCount = $(this)[0].files.length;
-//     var textbox = $(this).prev();
+$(document).on('change', '.file-input', function() {
+    var filesCount = $(this)[0].files.length;
+    var textbox = $(this).prev();
 
-//     if (filesCount === 1) {
-//         var fileName = $(this).val().split('\\').pop();
-//         textbox.text(fileName);
-//     } else {
-//         textbox.text(filesCount + ' files selected');
-//     }
-// });
-
-// vanilla js
-document.addEventListener('change', function (event) {
-    if (event.target.classList.contains('file-input')) {
-        var filesCount = event.target.files.length;
-        var textbox = event.target.previousElementSibling;
-  
-        if (filesCount === 1) {
-            var fileName = event.target.value.split('\\').pop();
-            textbox.textContent = fileName;
-        } else {
-            textbox.textContent = filesCount + ' files selected';
-        }
+    if (filesCount === 1) {
+        var fileName = $(this).val().split('\\').pop();
+        textbox.text(fileName);
+    } else {
+        textbox.text(filesCount + ' files selected');
     }
 });
+
+// // vanilla js
+// document.addEventListener('change', function (event) {
+//     if (event.target.classList.contains('file-input')) {
+//         var filesCount = event.target.files.length;
+//         var textbox = event.target.previousElementSibling;
+  
+//         if (filesCount === 1) {
+//             var fileName = event.target.value.split('\\').pop();
+//             textbox.textContent = fileName;
+//         } else {
+//             textbox.textContent = filesCount + ' files selected';
+//         }
+//     }
+// });
 
 
 

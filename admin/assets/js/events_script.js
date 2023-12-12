314790e27b804e9551fc4e9937ea31f793fe627a
franzxsu <2225292@slu.edu.ph>
2023-12-12 22:13:44 +0800

    get emails of attendees

--- admin/assets/js/events_script.js
+++ admin/assets/js/events_script.js
@@ -1,41 +1,46 @@
 
 const eventIDs = document.querySelectorAll('.eventReg');
+const eventIDsAtt = document.querySelectorAll('.eventAttended');
+const eventAttendEmail = document.querySelectorAll('.viewAttendedEmails');
+const emailContainer = document.getElementById("putEmailsHere");
 
 eventIDs.forEach((element) => {
-  const eventID = element.id.split('_')[1];
-  console.log(eventID);
-  fetch(`/registeredUsers/${eventID}`)
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById(`eventRegisteredFor_${eventID}`).innerText = data.rowCount;
-  })
+	const eventID = element.id.split('_')[1];
+	console.log(eventID);
+	fetch(`/registeredUsers/${eventID}`)
+		.then(response => response.json())
+		.then(data => {
+			document.getElementById(`eventRegisteredFor_${eventID}`).innerText = data.rowCount;
+		})
 });
 
-const eventIDsAtt = document.querySelectorAll('.eventAttended');
-const emailContainer = document.getElementById("attendedEmailContainer");
+
 
 eventIDsAtt.forEach((element) => {
-  const eventID = element.id.split('_')[1];
-  console.log(eventID);
-  fetch(`/attendedUsers/${eventID}`)
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById(`eventAttendedFor_${eventID}`).innerText = data.rowCount;
-  })
-
-  fetch(`/attendanceList/${eventID}`)
-  .then(response => response.json())
-  .then(response => {
-    console.log(response);
-
-    console.log(response.rows);
-    for (let i = 0; i < response.rows.length; i++) {
-      const elementEmail = document.createElement("p");
-      elementEmail.innerText = response.rows[i].email;
-      emailContainer.appendChild(elementEmail);
-    }
-  });
+	const eventID = element.id.split('_')[1];
+	console.log(eventID);
+	fetch(`/attendedUsers/${eventID}`)
+		.then(response => response.json())
+		.then(data => {
+			document.getElementById(`eventAttendedFor_${eventID}`).innerText = data.rowCount;
+		})
 });
 
+eventAttendEmail.forEach((element) => {
+	const eventID = element.id.split('_')[1];
+	console.log(eventID);
+	fetch(`/attendanceList/${eventID}`)
+		.then(response => response.json())
+		.then(response => {
+			console.log(response);
 
-
+			console.log("ROWS: "+response.rows.length +" FOR EVENT ID: "+eventID);
+			for (let i = 0; i < response.rows.length; i++) {
+				const elementEmail = document.createElement("p");
+				// console.log("FOR EVENT ID: "+eventID);
+				// console.log("ATTENDED: "+response.rows[i].email)
+				elementEmail.innerText = response.rows[i].email;
+				emailContainer.insertBefore(elementEmail, emailContainer.firstChild);
+			}
+		});
+});

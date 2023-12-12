c7cd2a8ef8baec5178036bcf3344a74883446403
franzxsu <2225292@slu.edu.ph>
2023-12-13 02:18:38 +0800

    get emails of attendees

--- admin/assets/js/events_script.js
+++ admin/assets/js/events_script.js
@@ -4,6 +4,8 @@ const eventIDsAtt = document.querySelectorAll('.eventAttended');
 const eventAttendEmail = document.querySelectorAll('.viewAttendedEmails');
 const emailContainer = document.getElementById(`putEmailsHere_<%= event.eventID %>`);
 
+const downloadButtons = document.querySelectorAll('.downloadEmailButton');
+
 eventIDs.forEach((element) => {
 	const eventID = element.id.split('_')[1];
 	console.log(eventID);
@@ -30,17 +32,53 @@ eventAttendEmail.forEach((element) => {
 	const eventID = element.id.split('_')[1];
 	console.log(eventID);
 	fetch(`/attendanceList/${eventID}`)
-		.then(response => response.json())
-		.then(response => {
-			console.log(response);
+	  .then(response => response.json())
+	  .then(response => {
+		console.log(response);
+  
+		console.log("ROWS: " + response.rows.length + " FOR EVENT ID: " + eventID);
+		for (let i = 0; i < response.rows.length; i++) {
+		  const elementEmail = document.createElement("p");
+		  elementEmail.innerText = response.rows[i].email;
+		  elementEmail.classList.add(`emailAttended_${eventID}`);
+		  document.getElementById(`putEmailsHere_${eventID}`).insertBefore(elementEmail, this.firstChild);
+		}
+	  });
+  });
+  
+  function initiateDownload(eventID) {
+	const emails = gatherEmails(eventID);
+	const csvContent = convertToCSV(emails);
+	// FILE NAME
+	const fileName = `emailsForEventId${eventID}.csv`;
+	const link = document.createElement("a");
+	link.setAttribute("href", csvContent);
+	link.setAttribute("download", fileName);
+	document.body.appendChild(link);
+	link.click();
+  }
+  
 
-			console.log("ROWS: "+response.rows.length +" FOR EVENT ID: "+eventID);
-			for (let i = 0; i < response.rows.length; i++) {
-				const elementEmail = document.createElement("p");
-				// console.log("FOR EVENT ID: "+eventID);
-				// console.log("ATTENDED: "+response.rows[i].email)
-				elementEmail.innerText = response.rows[i].email;
-				document.getElementById(`putEmailsHere_${eventID}`).insertBefore(elementEmail, this.firstChild);
-			}
-		});
-});
+  
+  downloadButtons.forEach((button) => {
+	button.addEventListener('click', (event) => {
+	  const clickedButtonID = event.target.id;
+	  const eventID = clickedButtonID.split('_')[1];
+	  initiateDownload(eventID);
+	});
+  });
+  
+  //get emails of attended for every eventid by getting html elements with the class emailAttended
+  function gatherEmails(eventID) {
+	const emailElements = document.querySelectorAll(`.emailAttended_${eventID}`);
+	const emails = Array.from(emailElements).map((element) => element.textContent.trim());
+	return emails;
+  }
+
+  //convert to csv replacve commas with newline
+  function convertToCSV(emails) {
+	const csvContent = "data:text/csv;charset=utf-8," +
+	  emails.map((email) => email.replace(",", ""))
+	  .join("\n");
+	return encodeURI(csvContent);
+  }
